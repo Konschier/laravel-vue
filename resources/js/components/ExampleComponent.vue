9e4acadfc1dad0dc62fb5ef90c7e12f530f56fd3
Maykon de Andrade Schier <maykonschier@gmail.com>
2020-07-05 17:15:43 -0300

    Apenas algumas modificações enquanto eu testo o vue junto do laravel

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Testando alguns recursos de desenvolvimento</div>
+
+                    <div class="card-body">
+                        Exemplaço da massa.
+                            <input type="text" v-model="test">
+                            {{test}}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: () => {
+           return{
+            test: ''
+        }
+            },
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
